67ee476a4acce13880fb1197d3255b0b108c35a0
renatadeac <renatadeac33@gmail.com>
2024-11-23 13:41:49 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DeacRenataLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DeacRenataLab7
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public ObservableCollection<Student> Students { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Students = new ObservableCollection<Student>
+            {
+                new Student { Name = "Ion Pop", Description = "Student la Informatica", ImagePath = "ion_pop.jpg" },
+                new Student { Name = "Marta Ionescu", Description = "Studentă la Matematică", ImagePath = "marta_ionescu.jpg" },
+                new Student { Name = "Andrei Alexandru", Description = "Student la Fizică", ImagePath = "andrei_alexandru.jpg" }
+            };
+
+            // Setează BindingContext-ul pentru a lega datele cu interfața
+            BindingContext = this;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = "Nicio descriere disponibilă";
+        public string ImagePath { get; set; } = string.Empty;
+    }
+}
+
