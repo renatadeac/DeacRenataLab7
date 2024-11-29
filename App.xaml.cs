4671ad41eea2cfcf1d4f5fa97e3e4d652bdcecc0
renatadeac <renatadeac33@gmail.com>
2024-11-29 18:08:54 +0200

    Laborator8 complet

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace DeacRenataLab7
+﻿using System;
+using DeacRenataLab7.Data;
+using System.IO;
+
+namespace DeacRenataLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
